f1cfbe86f098f1f0f606d91ddec25676a82af8b8
Leila Micaela Iglesias <91983284+leilalternativx@users.noreply.github.com>
2022-06-14 22:31:01 -0300

    Add files via upload

---
+++ src/CartWidget.js
@@ -0,0 +1,12 @@
+import './CartWidget.scss'
+import '@fortawesome/react-fontawesome'
+
+function CartWidget() {
+	return (
+		<div>
+			<FontAwesomeIcon icon="fa-solid fa-gamepad-modern" />
+		</div>
+	)
+}
+
+export default CartWidget;
